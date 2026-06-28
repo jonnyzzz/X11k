e498e188c5d45ecade58a6399860c3aba3f93cad
Eugene Petrenko <me@jonnyzzz.com>
2026-06-28 17:23:57 +0200

    Implement RENDER PictOpDisjointXor

--- src/main/kotlin/org/jonnyzzz/xserver/X11State.kt
+++ src/main/kotlin/org/jonnyzzz/xserver/X11State.kt
@@ -3007,6 +3007,22 @@ internal class X11State(
                     )
                     destinationFramebuffer.snapshotRegion(destinationX, destinationY, width, height)
                 }
+                XRender.OpDisjointXor -> {
+                    destinationFramebuffer.blendSolidDisjointXor(
+                        pixel = solid,
+                        destinationX = destinationX,
+                        destinationY = destinationY,
+                        width = width,
+                        height = height,
+                        clipRectangles = destination.clipRectangles,
+                        clipMask = destinationClipMask,
+                        mask = maskFramebuffer,
+                        maskX = maskX,
+                        maskY = maskY,
+                        maskAlphaAt = maskAlphaAt,
+                    )
+                    destinationFramebuffer.snapshotRegion(destinationX, destinationY, width, height)
+                }
                 XRender.OpXor -> {
                     destinationFramebuffer.blendSolidXor(
                         pixel = solid,
@@ -4198,6 +4214,15 @@ internal class X11State(
                     clipMask = destinationClipMask,
                 )
                 XRender.OpDisjointAtopReverse -> framebuffer.blendSolidDisjointAtopReverse(
+                    pixel = pixel,
+                    destinationX = rectangle.x,
+                    destinationY = rectangle.y,
+                    width = rectangle.width,
+                    height = rectangle.height,
+                    clipRectangles = destination.clipRectangles,
+                    clipMask = destinationClipMask,
+                )
+                XRender.OpDisjointXor -> framebuffer.blendSolidDisjointXor(
                     pixel = pixel,
                     destinationX = rectangle.x,
                     destinationY = rectangle.y,
